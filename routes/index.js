0edc78a9d36416286568bc36b19d815eb3042d53
kharman14 <harman.katelyn96@gmail.com>
2023-04-28 18:58:28 -0600

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
-// Lesson 1
-//const routes = require('express').Router();
-
-//const myController = require('../controllers');
-
-//routes.get('/', myController.awesomeFunction);
-//routes.get('/awesome', myController.returnAnotherPerson);
-
-//module.exports = routes;
-
-// Lesson 2
 const routes = require('express').Router();
-const userController = require('../controllers/user');
 
-routes.get('/', userController.getUser);
-routes.get('/username', userController.getUsername);
+const myController = require('../controllers');
+
+routes.get('/', myController.awesomeFunction);
+routes.get('/awesome', myController.returnAnotherPerson);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-// Lesson 1
-//var express = require('express');
-//var app = express();
-//const port = process.env.PORT || 3000;
-
-//app.use('/', require('./routes'));
-
-//app.listen(port, () => {
-//    console.log(`Server is running on port ${port}`);
-//});
-
-// Lesson 2
-const express = require('express');
-const app = express();
-const mongodb = require('./db/connect');
+var express = require('express');
+var app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
