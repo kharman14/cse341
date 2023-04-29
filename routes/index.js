f3dabe7d6ec26c32d32a137d9a3df5a294b5fbb3
kharman14 <harman.katelyn96@gmail.com>
2023-04-28 18:56:38 -0600

    undo to previous commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
+// Lesson 1
+//const routes = require('express').Router();
+
+//const myController = require('../controllers');
+
+//routes.get('/', myController.awesomeFunction);
+//routes.get('/awesome', myController.returnAnotherPerson);
+
+//module.exports = routes;
+
+// Lesson 2
 const routes = require('express').Router();
+const userController = require('../controllers/user');
 
-const myController = require('../controllers');
-
-routes.get('/', myController.awesomeFunction);
-routes.get('/awesome', myController.returnAnotherPerson);
+routes.get('/', userController.getUser);
+routes.get('/username', userController.getUsername);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var app = express();
+// Lesson 1
+//var express = require('express');
+//var app = express();
+//const port = process.env.PORT || 3000;
+
+//app.use('/', require('./routes'));
+
+//app.listen(port, () => {
+//    console.log(`Server is running on port ${port}`);
+//});
+
+// Lesson 2
+const express = require('express');
+const app = express();
+const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
